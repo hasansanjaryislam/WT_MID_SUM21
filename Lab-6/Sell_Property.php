ac569eaafcf3c6507f17bef6a27a8f023d16bd5a
Your Name <you@example.com>
2021-06-23 19:37:48 +0600

    Lab 6

---
+++ Lab-6/Sell_Property.php
@@ -0,0 +1,72 @@
+<html>
+	<body>
+		<form action="" method="post">
+		<h1>Sell Property</h1>
+			<table>
+			
+				<tr>
+					<td align="right">Property Location</td>
+					<td>
+					
+					<select name="P_location">
+						<option selected disabled>Select Location</option>
+						<option>Mirpur</option>
+						<option>Mohammadpur</option>
+						<option>Uttara</option>
+						<option>Banani</option>
+						<option>Gulshan</option>
+						<option>Dhanmondi</option>
+						<option>Baridhara</option>
+						<option>Bashundahara</option>
+						<option>Nikunjo</option>
+						<option>Kuril</option>
+						<option>Mogbazar</option>
+						<option>Rampura</option>
+					
+					</td>
+				</tr>
+				<tr>
+					<td align="right">Property Area</td>
+					<td>:<input name="P_Area" type="text" placeholder="Square feet"></td>
+					
+				</tr>
+				
+				<tr>
+					<td align="right">Property Type</td>
+					<td>
+					
+					<select name="P_Type">
+						<option selected disabled>Select Type</option>
+						<option>Apparment</option>
+						<option>House</option>
+						<option>Duplex</option>
+						<option>Plot</option>
+					
+					</td>
+				</tr>
+				
+				<tr>
+
+				<tr>
+					<td align="right">Property Id</td>
+					<td>:<input name="P_id" type="text" placeholder="Enter property id"></td>
+					
+				</tr>
+				</tr>
+				
+				<tr>
+					<td align="right">Property Price</td>
+					<td>:<input name="P_price" type="text" placeholder="Enter Property Price"></td>
+					
+				</tr>
+				
+				
+				<tr>
+					<td align="right"><input type="submit" value="Submit"></td>
+				</tr>
+				
+			</table>
+			
+		</form>
+	</body>
+</html>
